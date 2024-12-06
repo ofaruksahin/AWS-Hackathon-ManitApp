0a5fc5f58769cbdfa472c03d0b444b833d4c4cf3
Faruk Sahin <ofaruksahin@outlook.com.tr>
2024-12-06 17:02:46 +0300

    Seed worker disabled

--- ManitApp.API/Program.cs
+++ ManitApp.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ManitAppDbContext>();
 builder.Services.AddScoped<IVectorizeService, VectorizeService>();
-builder.Services.AddHostedService<SeedWorker>();
+//builder.Services.AddHostedService<SeedWorker>();
 
 var app = builder.Build();
 
